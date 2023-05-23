5797f43f8719cf747fc1e20710f4acc858637b0c
vengalasanjay <112609021+vengalasanjay@users.noreply.github.com>
2023-05-23 18:39:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "sanjay-final-poc-backend"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    region = "eu-west-2"
+    dynamodb_table = "poc-dynamodb-table"
   }
 }
